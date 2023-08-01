98d4bb8e0704a131bf722efaa74a219a1be73ca1
KFM06 <118633243+KFM06@users.noreply.github.com>
2023-08-01 09:07:51 -0230

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import folium
+m = folium.Map(location=[49.1852, -57.4184], zoom_start=12)
 st.title('Map Test')
-st.button('Click to view map')
+result = st.button('Click to view map')
+if result:
+  m.save('map.html')
