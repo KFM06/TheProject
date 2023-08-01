7867d6180d4841ea8c948fd8887975acca92400f
KFM06 <118633243+KFM06@users.noreply.github.com>
2023-08-01 09:32:09 -0230

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-import streamlit as st
 import folium
+import streamlit as st
+
 from streamlit_folium import st_folium
-st.title('Map Test')
-m = folium.Map(location=[49.1852, -57.4184], zoom_start=12)
-# call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
-st_map(result) = st_folium(map)
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[49.1852, -57.4184], zoom_start=16)
+folium.Marker(
+    [49.1852, -57.4184], popup="Deer Lake", tooltip="Deer Lake"
+).add_to(m)
+
+# call to render Folium map in Streamlit, but don't get any data back
+# from the map (so that it won't rerun the app when the user interacts)
+st_folium(m, width=725, returned_objects=[])
