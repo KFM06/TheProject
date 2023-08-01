2771c3bd595674183e9d6da87df130b9a3c33999
KFM06 <118633243+KFM06@users.noreply.github.com>
2023-08-01 10:24:20 -0230

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-import folium
-from streamlit_folium import st_folium, folium_static
+import streamlit as st
 
-m = folium.Map(location=[49.1852, -57.4184], 
-                 zoom_start=3, control_scale=True)
+st.set_page_config(
+  page_title="Test",
+)
 
-#Loop through each row in the dataframe
-for i,row in df.iterrows():
-    #Setup the content of the popup
-    iframe = folium.IFrame('Well Name:' + str(row["Well Name"]))
-    
-    #Initialise the popup using the iframe
-    popup = folium.Popup(iframe, min_width=300, max_width=300)
-    
-    #Add each row to the map
-    folium.Marker(location=[row[49.1852],row[-57.4184]],
-                  popup = popup, c=row['Well Name']).add_to(m)
-
-st_data = folium_static(m, width=700)
+st.title("Main Page")
+st.sidebar.success("Please select a page.")
